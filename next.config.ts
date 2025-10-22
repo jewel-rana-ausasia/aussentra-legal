00043011a28c36e226aaba46f7f69323a25679a7
Jewel Rana <ranaausasiagroup@gmail.com>
2025-10-22 16:03:56 +1100

    Upload type errors ignore command

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // This allows the build to complete even with type errors
+    ignoreBuildErrors: true,
+  },
   eslint: {
     // Warning: true will fail the build on ESLint warnings
     // Setting ignoreDuringBuilds: true will allow build to pass even with lint errors
